f421295b977e150b5a46618a7fbdfcbf2bb628bf
ricca1976 <astedalvivo@gmail.com>
2019-01-28 11:12:17 +0100

    Create userExist.php

---
+++ userExist.php
@@ -0,0 +1,42 @@
+
+
+<script>
+
+	
+	function userExists(){
+
+			var userName = "info4" ;
+			var comand = "userExists" ; 
+			
+	
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", "https://youbid.click/bidapi/CALL/app.php", true);
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			
+			xhr.onreadystatechange = function () {
+			if (this.readyState != 4) return;
+
+			if (this.status == 200) {
+				
+				
+			var data = JSON.parse(this.responseText) ;
+
+			 alert (data) ;
+			 console.log(data); 
+			
+			}
+			
+		
+			};
+
+			
+			xhr.send(JSON.stringify({
+			comand : comand , userName : userName  
+			}));	
+			
+
+			
+
+	}	
+
+</script>
